5218d233cb7b14312d8579a9402d525db07e034d
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-09-11 19:46:58 -0600

    import and register auth and main

--- build_a_basket/__init__.py
+++ build_a_basket/__init__.py
@@ -1,14 +1,20 @@
-#initial file that runs
+#how to run the app FLASK_APP=build_a_basket auto look for create and 
+#initial file that runs on the command flask run
 from flask import Flask
 
-#basic structure of how to create an app 
+#basic structure of how to create an instatnce of flask app 
 #only single instances go in init.py i.e. db, app creation etc
 def create_app():
     app = Flask(__name__)
 
-#import instance of the blueprint, and register it
-    from .main import main_blueprint
+#import instance (main/crud) of the blueprint, and register it
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+
+#import instance (auth) of the blueprint, and register it
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
 
